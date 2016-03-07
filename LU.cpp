a5e23b052adfd27efdb85b1fb6d14c34fbb55631
EMCsoftware <chomik711@interia.pl>
2016-03-07 18:39:36 +0100

    Dodane sortowanie, ogólne poprawki.
    Problem z przekazaniem vectora jako argument funkcji :(

--- LU.cpp
+++ LU.cpp
@@ -1,25 +1,51 @@
 /*     Librarian's Utility     */
 /*Udostepniany na licencji GNU General Public Licence*/
-/*Komentarze dodane w celach czysto edukacyjnych, ja sam ich nie za bardzo potrzebuje :P*/
-/*W zasadzie nie wiem...*/
-//Po co uzywam komentarzy wielowierszowych :D
 
-
-
-
-//                                                 Dodajemy odpowiednie biblioteki
-
-
-#include <iostream>//strumien wejscia/wyjscia konsoli (I/O - in/out, stream - strumien)
-#include <cstdio>//kilka przydatnych funkcji
-#include <string>//obsluga lancuchow tekstowych
-#include <windows.h>//pare uzytecznych funkcji, niestety sprawia, ze aplikacja staje sie nieprzenosna, dziala tylko na Windows
-#include <fstream>//operacje na plikach
-#include <vector>//dynamiczna tablica, inaczej kontener
-#include <conio.h>//kilka moliwosci konsoli
-#include <cstdlib>//zale¿y nam na wyjœciu z programu
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <windows.h>
+#include <fstream>
+#include <vector>
+#include <conio.h>
 using namespace std;
 //******************************************************
+class clientclass
+{
+      public:
+   vector <string> name;
+   vector <int> age;
+   vector <int> booksstored;
+   vector <int> borrowedever;
+   vector <int> lastborrow;
+   vector <string> adress;
+   vector <string> email;
+   vector <int> status;
+
+};
+
+                void sort( vector <string> taba[], vector <int> tabq[], vector <int> tabw[], vector <int> tabe[], vector <int> tabr[], vector <string> tabt[], vector <string> taby[], vector <int> tabu[], unsigned long long size )
+                     {
+                                 for( int i = 0; i < size; i++ )
+                                 {
+                                      for( int j = 0; j < size - 1; j++ )
+                                      {
+                                           if(  taba[j] > taba[j + 1]  )
+                                           {
+                                               swap( taba[ j ], taba[ j + 1 ] );
+                                               swap( tabq[ j ], tabq[ j + 1 ] );
+                                               swap( tabw[ j ], tabw[ j + 1 ] );
+                                               swap( tabe[ j ], tabe[ j + 1 ] );
+                                               swap( tabr[ j ], tabr[ j + 1 ] );
+                                               swap( tabt[ j ], tabt[ j + 1 ] );
+                                               swap( taby[ j ], taby[ j + 1 ] );
+                                               swap( tabu[ j ], tabu[ j + 1 ] );
+
+                                           }
+                                      }
+                                 }
+                     }
+
 void drawmenu(unsigned short posopt, unsigned long long poscli, string optlist[], vector <string>& clients)
 {
 cout<<"|=====================|=====================|"<<endl;
@@ -83,7 +109,7 @@ cout<<"|"<<endl;
 //powtarzamy ta czesc
 }
 
-cout<<"|=====================|=====================|"<<endl<<"Wazne! Wybierajac opcje 'Dodaj Uzytkownika' nie ma znaczenia, ktory klient jest wybrany w prawej kolumnie.\nKlawisz Esc-wyjscie";
+cout<<"|=====================|=====================|"<<endl<<"Wazne! Wybierajac opcje 'Dodaj Uzytkownika' nie ma znaczenia, ktory klient jest wybrany w prawej kolumnie.\nEsc-wyjscie.";
 }
 
 
@@ -91,7 +117,7 @@ cout<<"|=====================|=====================|"<<endl<<"Wazne! Wybierajac 
 void dodajuzytkownika()
 {
     char PL[64];
-    CharToOem("¹êŸæœ¿", PL);
+    CharToOem("Ä…Ä™ÅºÄ‡Å›Å¼", PL);
     system("cls");
     cout<<"#####################"<<endl
         <<"#                   #"<<endl
@@ -127,39 +153,38 @@ optionslist[4] = "Zmien status ksiazki";//                                4 elem
 vector <string> clients;// dynamiczna tablica ze spisem klientow biblioteki, przewaza nad zwyklymi tablicami tym, ze
 //w kazdej chwili mozna zmienic jej rozmiar
 
-vector <string> imie;
-vector <int> wiek;
-vector <int> nieoddaneksiazki;
-vector <int> ksiazkiwypozyczoneogolem;
-vector <int> dataostatniowypozyczonej;
-vector <string> adres;
-vector <string> email;
-vector <int> status;
 
-fstream wczytanedane;//zmienna przechowujaca plik
-wczytanedane.open("daneklientow.baza", ios::in | ios::out);//otwieramy plik (wczytaj/zapisz)
-if(wczytanedane.good() == true)//*************************wczytanie pliku******************************
+fstream getdata;//zmienna przechowujaca plik
+/*w zasadzie mowiac 'operacje na pliku' w C++ dzialamy na zmiennej do ktorej wczytalismy plik*/
+getdata.open("daneklientow.baza", ios::in | ios::out);//otwieramy plik (wczytaj/zapisz)
+if(getdata.good() == true)//*************************wczytanie pliku******************************
 {//gdy wczytywanie sie powiodlo...
-string danepobrane;//zmienna, do ktorej bedziemy wczytywac linijke tekstu z pliku
-while(!wczytanedane.eof())//powtarzamy, az dojdziemy do konca pliku...
+string dataloaded;//zmienna, do ktorej bedziemy wczytywac linijke tekstu z pliku
+while(!getdata.eof())//powtarzamy, az dojdziemy do konca pliku...
 {
-    getline(wczytanedane, danepobrane);//pobieramy linijke
-    clients.push_back(danepobrane);//dopisz do listy klientow nastepna linijke tekstu NA KONCU
-    imie = clients;
+    getline(getdata, dataloaded);//pobieramy linijke
+    clients.push_back(dataloaded);//dopisz do listy klientow nastepna linijke tekstu NA KONCU
+    clientclass client;
+    unsigned long long namesize = client.name.size();
+    sort(client.name, client.age, client.booksstored, client.borrowedever, client.lastborrow, client.adress, client.email, client.status, namesize);
+    //imie = clients;
 }
-wczytanedane.close();//zamykamy plik
+getdata.close();//zamykamy plik
 }else//w przeciwnym razie...
 {
-cout<<"\n\n\n\nPrzepraszamy, wystapil blad wczytawania pliku. Upewnij sie, ze:\n -plik 'daneklientow.baza' istnieje na dysku\n -posiadasz uprawnienia do odczytu pliku\n\nNacisnj dowolny klawiz, aby zamknac program."<<endl<<endl<<endl<<endl;
+cout<<"\n\n\n\nPrzepraszamy, wystapil blad wczytawania pliku. Upewnij sie, ze:\n -plik 'imionaklientow.baza' istnieje na dysku\n -posiadasz uprawnienia do odczytu pliku\n\nNacisnj dowolny klawiz, aby zamknac program."<<endl<<endl<<endl<<endl;
 getchar();//czekaj na dowolny klawisz
-exit(1);
 /*Po wcisnieciu klawisza program konczy dzialanie*/
 }
+
+
+
+
 //*************************************************koniec wczytywania****************
 unsigned short menuoptionsposition = 1;
 unsigned long long menuclientsposition = 1;//Jak myslicie, chyba nie bedzie klientow wiecej niz 18446744073709551615 :D
 bool menulevel = 0;//czy jestesmy w menu lewym czy prawym
-unsigned char key;
+unsigned char znak;
 bool options = 0;
 
 do
@@ -167,8 +192,8 @@ do
   system("cls");//czyscimy konsole
   drawmenu(menuoptionsposition, menuclientsposition, optionslist, clients);  //rysujemy menu, przekazujemy mu, ktore opcje sa zaznaczone
   options = 0;
-  key = getch();//z biblioteki Conio
-switch (key)
+  znak = getch();//z biblioteki Conio
+switch (znak)
 {
 case 72://strzalka w gore
 if(menulevel == 0)
@@ -249,7 +274,6 @@ case 77://strzalka w prawa
         options = 1;
         break;
     }
-
 case 27://Esc
 
  system("cls");
@@ -260,8 +284,9 @@ case 27://Esc
  exit(1);
  else if(yesno == 110)//n
  menu();
+
 }
-        key = 0;
+        znak = 0;
 
 
 
